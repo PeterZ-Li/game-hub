b457152252d3cff2dac15e0ef6f948e9108b5631
PeterZ-Li <1846136440@qq.com>
2023-08-10 18:17:35 +0800

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,30 @@ import GenereList from "./components/GenreList"
 import NavBar from "./components/NavBar"
 
 function App() {
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"` //1024px
-  }}>
-    <GridItem area='nav'>
-      <NavBar />
-    </GridItem>
-    <Show above="lg">
-      <GridItem area='aside'>
-        <GenereList />
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"` //1024px
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr'
+      }}
+    >
+      <GridItem area='nav'>
+        <NavBar />
       </GridItem>
-    </Show>
-    <GridItem area='main'>
-      <GameGrid />
-    </GridItem>
-  </Grid>
+      <Show above="lg">
+        <GridItem area='aside' paddingX={5}>
+          <GenereList />
+        </GridItem>
+      </Show>
+      <GridItem area='main'>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  )
 }
 
 export default App
